3fce52d5df99fc296f465f10dfd051a473d04429
Michael Chow <machow@princeton.edu>
2017-03-27 09:19:36 -0400

    setup olympics db in requirements

--- requirements.sh
+++ requirements.sh
@@ -2,7 +2,9 @@ git clone https://$GITHUB_TOKEN@github.com/datacamp/courses-intro-to-sql.git
 
 service postgresql start \
   && sudo -u postgres createdb -O repl films \
+  && sudo -u postgres createdb -O repl olympics \
   && cd courses-intro-to-sql \
   && sudo -u postgres psql films < data/films/films.sql \
+  && sudo -u postgres psql olympics < data/olympics/code/olympics.sql \
   && service postgresql stop
 
