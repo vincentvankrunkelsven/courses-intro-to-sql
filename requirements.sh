62df8eaeb10bdaa217de5705ae24440ed64a533d
Colin Ricardo <colinricardo@users.noreply.github.com>
2017-05-24 11:16:10 -0400

    removed nycbikes

--- requirements.sh
+++ requirements.sh
@@ -1,13 +1,13 @@
 BASE_DIR=$(pwd)
-NYCBIKES_DATA="https://assets.datacamp.com/course/sql/nycbikes15.zip"
-NYCBIKES_REPO="https://assets.datacamp.com/course/tmp_fixme_filip/nycbikes15_db_scripts.zip"
+#NYCBIKES_DATA="https://assets.datacamp.com/course/sql/nycbikes15.zip"
+#NYCBIKES_REPO="https://assets.datacamp.com/course/tmp_fixme_filip/nycbikes15_db_scripts.zip"
 FILMS_REPO="https://assets.datacamp.com/course/tmp_fixme_filip/films.zip"
 
 apt-get update && apt-get install -y unzip
 
 # get repos
-wget $NYCBIKES_REPO
-unzip nycbikes15_db_scripts.zip
+#wget $NYCBIKES_REPO
+#unzip nycbikes15_db_scripts.zip
 
 mkdir -p courses-intro-to-sql/data
 wget $FILMS_REPO
@@ -17,17 +17,17 @@ unzip films.zip -d courses-intro-to-sql/data
 #git clone https://$GITHUB_TOKEN@github.com/datacamp/nycbikes15.git
 
 # get data for nycbikes15
-wget $NYCBIKES_DATA
-unzip nycbikes15.zip -d ./nycbikes15/data
+#wget $NYCBIKES_DATA
+#unzip nycbikes15.zip -d ./nycbikes15/data
 
 # build databases
 service postgresql start \
   && sudo -u postgres createdb -O repl films \
-  && sudo -u postgres createdb -O repl nycbikes15 \
+ # && sudo -u postgres createdb -O repl nycbikes15 \
   && cd courses-intro-to-sql \
   && sudo -u postgres psql films < data/films/films.sql \
-  && cd $BASE_DIR/nycbikes15 \
-  && sudo -u postgres psql nycbikes15 < sql-setup/create-db-postgres.sql \
+  #&& cd $BASE_DIR/nycbikes15 \
+  #&& sudo -u postgres psql nycbikes15 < sql-setup/create-db-postgres.sql \
   && service postgresql stop
 
 pip3 install git+https://github.com/datacamp/sqlwhat.git@staging
